4241ce21be0f3821d5a3e632b766a7f9eb5fba0c
noeliamorato <cbbe.noelia.morato.ca@unifranz.edu.bo>
2023-05-04 22:27:20 -0400

    version 28-04_2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 class Cancion:
     def __init__(self,titulo,categoria,idioma):
         self.titulo=titulo
@@ -6,18 +6,28 @@ class Cancion:
         self.idioma=idioma
 
 app = Flask(__name__)
+cancion1= Cancion('La guitarra', 'Pop', 'Castellano')
+cancion2= Cancion('Para no verte más', 'Pop', 'Castellano')
+cancion3= Cancion('Balada para un gordo', 'Balada', 'Castellano')
+lista=[cancion1, cancion2, cancion3]
 
 @app.route('/')
 def index():
-    cancion1= Cancion('La guitarra', 'Pop', 'Castellano')
-    cancion2= Cancion('Para no verte más', 'Pop', 'Castellano')
-    cancion3= Cancion('Balada para un gordo', 'Balada', 'Castellano')
-    lista=[cancion1, cancion2, cancion3]
+    
     return render_template('listar.html', titulo = 'Canciones', musicas=lista)
 
 @app.route("/nuevoregistro")
 def nuevoregistro():
     return render_template('nuevoRegistro.html', titulo = 'Nueva Canción')
+
+@app.route('/crear', methods=['POST',])
+def crear():
+    titulo = request.form['titulo']
+    categoria = request.form['categoria']
+    idioma = request.form['idioma']
+    cancion = Cancion(titulo, categoria, idioma)
+    lista.append(cancion)
+    return render_template('listar.html', titulo='Canciones', musicas=lista)
 
 app.run(debug=False,host="0.0.0.0", port=5000)
 
