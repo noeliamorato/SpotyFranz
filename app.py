e8fc2bb5063696fc681ab9b43e03cfc94d680539
noeliamorato <cbbe.noelia.morato.ca@unifranz.edu.bo>
2023-05-26 08:51:52 -0400

    26/05/2023_01

--- app.py
+++ app.py
@@ -1,89 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+# coding: utf-8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-class Usuario:
-    def __init__(self, nombre, nickname, clave):
-        self.nombre = nombre
-        self.nickname = nickname
-        self.clave = clave
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
-usuario1 = Usuario('Jair Sampaio', 'JS', 'patitofeo')
-usuario2 = Usuario('Rosa Flores', 'rosita', 'michifuz')
-usuario3 = Usuario('Yami Moto Nokamina', 'kamikaze', 'sayonara')
-usuarios = {
-    usuario1.nickname: usuario1,
-    usuario2.nickname: usuario2,
-    usuario3.nickname: usuario3
-}
+db = SQLAlchemy(app)
 
-class Cancion:
-    def __init__(self,titulo,categoria,idioma):
-        self.titulo=titulo
-        self.categoria = categoria
-        self.idioma = idioma
+from views import *
 
-app = Flask (__name__)
-app.secret_key = 'cochabamba'
-
-cancion1= Cancion('La guitarra','Pop','Castellano')
-cancion2= Cancion('Para no verte mas','Pop','Castellano')
-cancion3= Cancion('Balada para un gordo','Balada','Castellano')
-lista = [cancion1, cancion2, cancion3]
-
-@app.route('/')
-
-def index():
-    
-
-    return render_template('listar.html', titulo='Canciones', musicas=lista)
-
-@app.route('/nuevoregistro')
-def nuevoregistro():
-    if 'usuario_logueado' not in session or session['usuario_logueado'] is None:
-        flash('Usuario no conectado.')
-        return redirect(url_for('login', proxima=url_for('nuevoregistro')))
-    else:
-        flash('Usuario conectado.')
-        return render_template('nuevoRegistro.html', titulo='Nueva Canción')
-
-
-@app.route('/crear', methods=['POST', ])
-def crear():
-    titulo = request.form['titulo']
-    categoria = request.form['categoria']
-    idioma = request.form['idioma']
-    cancion = Cancion(titulo, categoria, idioma)
-    lista.append(cancion)
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    proxima=request.args.get('proxima')
-    return render_template('login.html',proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST',])
-def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
-        if request.form['clave'] == usuario.clave:
-            session['usuario_logueado'] = usuario.nickname
-            flash(usuario.nickname + ' ¡conectado con éxito!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-        else:
-            flash('Usuario no conectado.')
-            return redirect(url_for('login', proxima = url_for('nuevoregistro')))
-    else:
-        flash('Usuario no conectado.')
-        return redirect(url_for('login', proxima = url_for('nuevoregistro')))
-
-
-@app.route('/logout')
-def logout():
-    session['usuario_logueado'] = None
-    flash('¡Logout efectuado exitosamente!')
-    return redirect(url_for('index'))
-
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
